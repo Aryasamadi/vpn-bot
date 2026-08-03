404390334ba13a4ae633653571264788814d326b
Aryasamadi <167288822+Aryasamadi@users.noreply.github.com>
2026-08-03 12:56:07 +0000

    update start

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ CF_KV_ID = os.getenv("CF_KV_ID", "")
 # متغیر محیطی برای دامنه اصلی ربات
 APP_BASE_URL = os.getenv("APP_BASE_URL", "https://technowvpnbot.ariyacompany-io.workers.dev")
 
-# بنر آگهی در صدر سابلینک (کانفیگ نامعتبر نمایشی) ثابت شد - تغییر یافته طبق درخواست شما
-BANNER_CONFIG = "ss://none:1234@1.1.1.1:443#%F0%9F%8C%90%D9%87%D8%B1%20%D8%B1%D9%88%D8%B2%20%D8%B3%D8%A7%D8%A8%D9%84%DB%8C%D9%86%DA%A9%20%D8%AE%D9%88%D8%AF%20%D8%B1%D8%A7%20%D8%A2%D9%BE%D8%AF%DB%8C%D8%AA%20%DA%A9%D9%86%DB%8C%D8%AF%20%E2%9A%A1"
+# بنر آگهی در صدر سابلینک (کانفیگ نامعتبر نمایشی) ثابت شد
+BANNER_CONFIG = "vless://89210719-c3b9-4053-9c75-c0c3396fabd3@Update:7878?encryption=none&security=none&type=ws&path=%2F#%F0%9F%93%A2%D9%87%D8%B1%20%D8%B1%D9%88%D8%B2%20%D9%84%DB%8C%D9%86%DA%A9%20%D8%AE%D9%88%D8%AF%20%D8%B1%D8%A7%20%D8%A8%D8%B1%D9%88%D8%B2%D8%B1%D8%B3%D8%A7%D9%86%DB%8C%20%DA%A9%D9%86%DB%8C%D8%AF%F0%9F%93%A2"
 
 CF_HEADERS = {
     "Authorization": f"Bearer {CF_API_TOKEN}",
@@ -1720,8 +1720,7 @@ async def handle_sublink(token: str):
     cached_payload = await get_kv("cached_configs_payload")
 
     if cached_payload is None:
-        # تغییر ایجاد شده طبق درخواست شما: اضافه شدن ORDER BY id DESC برای قرارگیری کانفیگ‌های جدید در ابتدای لیست
-        cfg_res = await query_db("SELECT config_text FROM configs WHERE is_active = 1 ORDER BY id DESC")
+        cfg_res = await query_db("SELECT config_text FROM configs WHERE is_active = 1")
         confs = get_rows(cfg_res)
         
         payload_lines = []
